85f06f6240269873d5d65082a2f27158f9a751da
devmashine <kurdalin.hi@gmail.com>
2020-05-30 06:58:51 +0300

    remove db.sqlite3 from folder and fix problem with table in buyer account page

--- celery_app/views.py
+++ celery_app/views.py
@@ -30,7 +30,6 @@ import datetime
 
 
 
-
 class userGroups:
     def sellers(self):
         return User.objects.filter(groups = 1)
@@ -75,14 +74,30 @@ class Account_page(userGroups, View):
         # render for Sellers
         for i in sellers_into_group:
             if self.request.user.id == i.id:
+
                 print('account seller')
+
+
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+                
 
                 user_seller_auctions = Auctions.objects.filter(seller = self.request.user).order_by('-start_auction')
         
-
-
-
-
 
                 # count the bids at auction
                 bids = []
@@ -117,7 +132,6 @@ class Account_page(userGroups, View):
                 }
 
         # render for Buyers
-
         buyers_into_group = userGroups.buyers(self)
 
         for i in buyers_into_group:
@@ -129,17 +143,35 @@ class Account_page(userGroups, View):
                 user_auctions_short_from_prices = Prices.objects.filter(buyer_id = self.request.user.id, winner= 1, auction__status = 3).select_related('auction').prefetch_related('auction__seller')
 
                 
-                user_auctions_short_from_prices_actual = Prices.objects.filter(buyer_id = self.request.user.id, auction__status = 2).select_related('auction').prefetch_related('auction__seller')
-
+                user_auctions_short_from_prices_actual = Prices.objects.select_related('auction').filter(buyer_id = self.request.user.id, auction__status = 2).prefetch_related('auction__seller').order_by('-new_price_time')
                 
+                list_id = [0]
+                unique_actual_auctions_for_buyer = []
+                for auctions in  user_auctions_short_from_prices_actual:
+                    print('auctions.auction.id', auctions.auction.id)
+                    for i in list_id:
+                        print('i ', i, ' and auctions.auction.id ', auctions.auction.id)
+                        if auctions.auction.id != i:
 
-                print('user_auctions_short_from_prices', user_auctions_short_from_prices)
+                            print('i into if', i)
+                            unique_actual_auctions_for_buyer.append(auctions)
+                            print('unique_actual_auctions_for_buyer', unique_actual_auctions_for_buyer)
+
+                            list_id = []
+                            list_id.append(auctions.auction.id)
+                            
+
+                    auctions_id = auctions.auction.id
+                    list_id.append(auctions_id)
+
+                print('unique_actual_auctions_for_buyer ', unique_actual_auctions_for_buyer)
 
 
                 
                 ctx = {
                     'form': form,
                     'buyers_into_group': buyers_into_group,
+                    'unique_actual_auctions_for_buyer': unique_actual_auctions_for_buyer,
 
                     'user_auctions_short_from_prices_actual': user_auctions_short_from_prices_actual,
                     'user_auctions_short_from_prices': user_auctions_short_from_prices,
